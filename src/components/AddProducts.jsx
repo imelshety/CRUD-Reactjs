4ebbd010098e0e81fb45bdd8272f344f2ce5381c
imelshety <mohamedkingman99@gmail.com>
2023-09-10 23:26:07 +0300

    display data from AddProduct component

--- src/components/AddProducts.jsx
+++ src/components/AddProducts.jsx
@@ -1,11 +1,108 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AddProducts = () => {
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState(0);
+    const [description, setDescription] = useState('');
+    const [brand, setBrand] = useState('');
+    const [id, setId] = useState(0);
+
+    let navigate = useNavigate('');
+    const formSubmit = (e) => {
+        e.preventDefault();
+        axios.post('https://dummyjson.com/products/add', {
+            title, // title : title in E6 writes title  directly
+            price,
+            description,
+            brand,
+            id
+            /* other product data */
+        }).then((data) => console.log(data));
+        navigate('/products', { state: { title, price, description, brand, id } })
+    }
     return (
-        <div className="h-screen w-full mt-20 flex flex-col items-center ">
-            <h2 className="text-green-400 font-bold text-4xl">ADD Product</h2>
-            <button className="inline-block rounded border border-current px-8 py-3 text-sm font-medium text-indigo-600 transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-indigo-500">ADD</button>
+        <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+            <div className="mx-auto max-w-lg">
+                <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">
+                    Add Product Details
+                </h1>
+                <form
+                    onSubmit={formSubmit}
+                    className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
+                >
+                    <div className="relative">
+                        <label htmlFor="id" className="sr-only">Product ID</label>
+                        <input
+                            required
+                            type="boolean"
+                            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                            placeholder="Id of the Product"
+                            onChange={(e) => setId(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <div className="relative">
+                            <label htmlFor="text" className="sr-only">Product Name</label>
+                            <input
+                                required
+                                type="text"
+                                className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                                placeholder="Title of The Product"
+                                onChange={(e) => setTitle(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="price" className="sr-only">Price</label>
+                        <div className="relative">
+                            <input
+                                required
+                                type="boolean"
+                                className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                                placeholder="price of The Product"
+                                onChange={(e) => setPrice(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="relative">
+                            <label htmlFor="text" className="sr-only">Product brand</label>
+                            <input
+                                required
+                                type="text"
+                                className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                                placeholder="brand Name of the Product"
+                                onChange={(e) => setBrand(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="id" className="sr-only">description</label>
+                        <div className="relative">
+                            <textarea
+                                rows="4"
+                                cols="50"
+                                required
+                                name="description"
+                                className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                                placeholder="description of The Product"
+                                onChange={(e) => setDescription(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <button
+                        type="submit"
+                        className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white transition hover:scale-105"
+                    >
+                        ADD Product
+                    </button>
+                </form>
+            </div>
         </div>
     )
 }
 
 export default AddProducts;
+{/* <button className="inline-block rounded border border-current px-8 py-3 text-sm font-medium text-indigo-600 transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-indigo-500">ADD</button> */ }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,20 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import { getProducts } from "./Api/Api";
 import { useEffect, useState } from "react";
 
 import Swal from 'sweetalert2'
 const Products = () => {
+    const { state } = useLocation();
+
     const [products, setProducts] = useState([])
     useEffect(() => {
         fetchProducts();
+        console.log(state);
     }, []);
     const fetchProducts = async () => {
         const productData = await getProducts();
-        setProducts(productData);
+        setProducts([...productData, state]);
+
     };
     const deleteHandleProduct = (product) => {
         Swal.fire({
@@ -32,9 +36,7 @@ const Products = () => {
                         prevProducts.filter((productId) => productId.id !== product.id)
                     ))
                 })
-
         })
-
     };
     return (
         <div className="w-full h-screen flex">
@@ -69,9 +71,9 @@ const Products = () => {
 
                 <tbody className="divide-y divide-gray-200 py-8">
                     {
-                        products.slice(0, 18).map((product) => {
+                        products.map((product) => {
                             return (
-                                <tr className="flex justify-center" key={product.id}>
+                                <tr className="flex justify-center" key={product.id} >
                                     <td className="w-1/4 whitespace-nowrap text-center py-2 font-medium text-gray-900">
                                         {product.id}
                                     </td>
@@ -86,7 +88,6 @@ const Products = () => {
                                     </td>
                                     <td className="whitespace-nowrap text-center py-2 text-gray-700">
                                         <Link to={`/products/${product.id}`} className="rounded border border-current ml-2 px-5 py-2 text-sm font-medium text-indigo-600 transition hover:scale-110 hover:shadow-xl focus:outline-indigo-700 active:text-red-500">View</Link>
-                                        <button className="rounded border border-current ml-2 px-5 py-2 text-sm font-medium text-indigo-600 transition hover:scale-110 hover:shadow-xl focus:outline-indigo-700 active:text-red-500">Edit</button>
                                         <button className="rounded border border-current ml-2 px-5 py-2 text-sm font-medium text-red-600 transition hover:scale-110 hover:shadow-xl focus:outline-red-600 active:text-indigo-500" onClick={() => deleteHandleProduct(product)}>Delete</button>
 
                                     </td>
